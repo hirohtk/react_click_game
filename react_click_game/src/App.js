80e3f7e46f0c3fc2531882aed8fa1f9323116842
Kensen <hirohtk@gmail.com>
2020-01-17 20:18:49 -0800

    added randomization function for pictures, but need to restore hover functionality

--- react_click_game/src/App.js
+++ react_click_game/src/App.js
@@ -8,9 +8,12 @@ import pictures from "./pictures.json"
 
 class App extends React.Component {
 
+  // state controls ALL components below.  If needing to change state on a particular component, need to use an array for that state property
   state = {
     pictures,
     hoverClass: [],
+    pointsScored: 0,
+    picturesClicked: [],
   }
 
   hover = id => {
@@ -26,19 +29,53 @@ class App extends React.Component {
     }
     // this new array will be made every time you hover, and the particular index of this array that matches the id
     // of the card being hovered will be "hover", which will trigger the css for transforming.
-    this.setState({hoverClass: newArray})
+    this.setState({ hoverClass: newArray })
     console.log(newArray)
 
   }
 
   unhover = () => {
     // setting the entire array that handles the css classname for the hover transform to "".  no ID needed
-    this.setState({hoverClass: ""})
+    this.setState({ hoverClass: "" })
   }
 
   test = (id) => {
     // this can become part of the basis for the game
     console.log(id);
+  }
+
+  componentDidMount() {
+    this.randomize();
+  }
+
+  randomize = () => {
+    let newArray = [];
+    let cap = this.state.pictures.length;
+    // cap var is here to increase the length that the for loop runs, because.. my if statement "skips" an iteration if satisfied
+    // starting cap at 9, and increasing by one if we ever "skip" 
+    for (let i = 0; i <= cap; i++) {
+      // RANDOM NUMBER is 0 through 8
+      var random = Math.floor(Math.random() * 9);;
+      console.log("new loop");
+
+       // If this new array has nine members, end this function by setting state
+      if (newArray.length === 9) {
+        console.log(newArray)
+        this.setState({pictures: newArray});
+      }
+
+      else {
+        if (newArray.includes(this.state.pictures[random])) {
+          //console.log("existing instance, skipping...")
+          cap++;
+          continue;
+        }
+        else {
+          console.log(`pushing picture ${random} into array`);
+          newArray.push(this.state.pictures[random]);
+        }
+      }
+    }
   }
 
   render() {
@@ -50,38 +87,38 @@ class App extends React.Component {
           <p>Yup.</p>
           {
             // this is why .map() works- the way an array is rendered is quite simple, it's just all of the array values
-            [1,2,3,4,5]
+            [1, 2, 3, 4, 5]
           }
         </div>
         <div className="outerCardsContainer">
-        <div className="cardsContainer">
-        {
-          // for each in pictures (WHICH IS AN ARRAY), give me an array of <Cards with these props inside> </Cards>
-          this.state.pictures.map(pics =>
-            
-            <Cards
-            route={pics.route}
-            id={pics.id}
+          <div className="cardsContainer">
+            {
+              // for each in pictures (WHICH IS AN ARRAY), give me an array of <Cards with these props inside> </Cards>
+              this.state.pictures.map(pics =>
 
-            hover={this.hover}
-            unhover={this.unhover}
-            // using the hoverClass array, re-use the id already coming from the json file 
-            hoverClass={this.state.hoverClass[pics.id - 1]}
+                <Cards
+                  route={pics.route}
+                  id={pics.id}
 
-            key={pics.id}
+                  hover={this.hover}
+                  unhover={this.unhover}
+                  // using the hoverClass array, re-use the id already coming from the json file 
+                  hoverClass={this.state.hoverClass[pics.id - 1]}
 
-            test={this.test}
-            test2={this.test2}
-            >
-            </Cards>
-            
-          )
-        }
-        </div>
+                  key={pics.id}
+
+                  test={this.test}
+                  test2={this.test2}
+                >
+                </Cards>
+
+              )
+            }
+          </div>
         </div>
       </Wrapper>
     );
   }
 
 }
-  export default App;
+export default App;
