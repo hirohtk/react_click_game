309329bf4ed2aa24a0067fc3a12370424280100c
Kensen <hirohtk@gmail.com>
2020-01-23 21:51:53 -0800

    Updates

---
+++ precache-manifest.b12643c1ece4015b1fdf73c1aee70144.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f94e353ec02ce174a9b8f494a088300",
+    "url": "/react_click_game/index.html"
+  },
+  {
+    "revision": "11f21bf3424231556f72",
+    "url": "/react_click_game/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "bafae148ce6eef92cb3e",
+    "url": "/react_click_game/static/css/main.4824bc39.chunk.css"
+  },
+  {
+    "revision": "11f21bf3424231556f72",
+    "url": "/react_click_game/static/js/2.46ec4f21.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react_click_game/static/js/2.46ec4f21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bafae148ce6eef92cb3e",
+    "url": "/react_click_game/static/js/main.3a03ed90.chunk.js"
+  },
+  {
+    "revision": "4d452e6531cd827af07a",
+    "url": "/react_click_game/static/js/runtime-main.0c00a474.js"
+  },
+  {
+    "revision": "88ae609b91504524690dee25bca8f445",
+    "url": "/react_click_game/static/media/horizontalnetwork.88ae609b.png"
+  }
+]);
